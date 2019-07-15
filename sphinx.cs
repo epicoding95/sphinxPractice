21023d22ed14d69cd9aa6cb9c369244a39439f01
Erik Irgens and Kelar Crisp <erik@>
2019-07-15 14:58:25 -0700

    Finish sphinx game.

--- sphinx.cs
+++ sphinx.cs
@@ -4,44 +4,41 @@ using System.Collections.Generic;
 class Sphinx
 {
   
+    public static Dictionary <int, string> myRiddles = new Dictionary <int,string>() { {1,"This walks on four legs in the morning, two legs in the afernoon, and three legs in the evening. What am I?"},{2, "If two peacocks lay two eggs in two days how many eggs does one peacock lay in four days?"}, {3, "I have a bed, but I do not sleep. I have a mouth, but I don't eat. You hear me whisper, but I never talk. You can see me run, I never walk. What am I?"} };
+
+    public static Dictionary <int, string> myAnswers = new Dictionary <int,string>() { {1,"A person"},{2, "Peacocks don't lay eggs."}, {3, "A river"} };
+
+    public static List<int> randomNumbers = new List<int>{};
+
     static void Main()
     {
-        
-        
-
-        Dictionary <int, string> myRiddles = new Dictionary <int,string>() { {1,"This walks on four legs in the morning, two legs in the afernoon, and three legs in the evening. What am I?"},{2, "riddles2"}, {3, "riddles3"} };
-
-        Dictionary <int, string> myAnswers = new Dictionary <int,string>() { {1,"A person"},{2, "answer2"}, {3, "answer3"} };
-
-        Dictionary <int, int> randomNumbers = new Dictionary <int,int>() {  };
-        
-        
-        for (int i = 0; i < myRiddles.Count; i ++) {
-            Random rnd = new Random();
-             int randomizer  = rnd.Next(1, 3);
-            if (randomNumbers.ContainsKey(i)) {
-                
-                randomNumbers.Add(randomizer, randomizer);
-            } 
-            else {
-            randomNumbers.Add(i, i);
-            }
-            Console.WriteLine(randomNumbers);
-        }
-
         
         Console.WriteLine("MAIN MENU");
         Console.WriteLine("Do you want to play our game?");
         string answer = Console.ReadLine();
         if (answer == "yes" || answer == "Yes")
-        {
-            Console.WriteLine(myRiddles[randomizer]);
+        {   
+            randomGenerator();
+            Console.WriteLine(myRiddles[randomNumbers[0]]);
             string answer1 = Console.ReadLine();
-            if (answer1 == myAnswers[randomizer]){
+            if (answer1 == myAnswers[randomNumbers[0]]){
                 Console.WriteLine("YOU'RE CORRECT");
-                Main();
+
+                Console.WriteLine(myRiddles[randomNumbers[1]]);
+                string answer2 = Console.ReadLine();
+                if (answer2 == myAnswers[randomNumbers[1]]){
+                    Console.WriteLine("YOU'RE CORRECT");
+                    
+                    Console.WriteLine(myRiddles[randomNumbers[2]]);
+                    string answer3 = Console.ReadLine();
+                    if (answer3 == myAnswers[randomNumbers[2]]){
+                    Console.WriteLine("YOU'RE CORRECT");
+                    Console.WriteLine("You have slain me. *DIES*");
+
+                }
             }else{
                 Console.WriteLine("you're wrong");
+                Console.WriteLine("*EATS YOU*");
                 Main();
             }
 
@@ -51,4 +48,23 @@ class Sphinx
 
         }
     }
+    }
+
+        static void randomGenerator(){
+
+            Random rnd = new Random();
+            for (int i = 0; i < 3; i++){
+                int number = rnd.Next(1, 4);
+                while (randomNumbers.Contains(number)){
+                number = rnd.Next(1, 4);
+                }
+                randomNumbers.Add(number);
+                
+            }
+           
+        }
+        
 }
+
+
+  
